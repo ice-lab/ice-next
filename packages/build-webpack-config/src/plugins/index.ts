96c4fa64e43354aa4d615019854d5a42a5d1d4cf
ClarkXia <xiawenwu41@gmail.com>
2022-03-09 14:36:17 +0800

    fix: types

--- packages/build-webpack-config/src/plugins/index.ts
+++ packages/build-webpack-config/src/plugins/index.ts
@@ -1,7 +1,8 @@
 import swcPlugin from './swc.js';
 import type { Config } from '@ice/types';
+import type { UnpluginOptions } from 'unplugin';
 
-const getTransformPlugins = (rootDir: string, config: Config) => {
+const getTransformPlugins = (rootDir: string, config: Config): UnpluginOptions[] => {
   const { sourceMap, transformPlugins = [], transforms = [] } = config;
   return [
     swcPlugin({ rootDir, sourceMap }),
