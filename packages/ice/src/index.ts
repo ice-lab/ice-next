a90d4c2caab1026043b07197ea2028f8701ad3d0
luhc228 <luhengchang228@gmail.com>
2022-03-11 14:08:58 +0800

    refactor: service route generate

--- packages/ice/src/index.ts
+++ packages/ice/src/index.ts
@@ -2,16 +2,15 @@ import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { Context } from 'build-scripts';
 import consola from 'consola';
-import { generateRouteManifest, formatNestedRouteManifest } from '@ice/route-manifest';
 import Generator from './service/runtimeGenerator.js';
 import preCompile from './service/preCompile.js';
 import createWatch from './service/watchSource.js';
 import start from './commands/start.js';
 import build from './commands/build.js';
+import { generateRoutesRenderData, getRoutesData } from './routes.js';
 import type { CommandArgs, CommandName, IGetBuiltInPlugins } from 'build-scripts';
 import type { ExportData } from '@ice/types/esm/generator.js';
 import type { ExtendsPluginAPI } from '@ice/types/esm/plugin.js';
-import type { RouteManifest, NestedRouteManifest } from '@ice/route-manifest';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -29,9 +28,8 @@ async function createService({ rootDir, command, commandArgs, getBuiltInPlugins 
   const { addWatchEvent, removeWatchEvent } = createWatch(path.join(rootDir, 'src'), command);
   const tmpDirName = '.ice';
 
-  const { routeManifest, routes } = getRouteData(rootDir);
-  const componentsImportStr = generateComponentsImportStr(routeManifest);
-  const routesStr = generateRoutesStr(routes);
+  let { routes, routeManifest } = getRoutesData(rootDir);
+  const { componentsImportStr, routesStr } = generateRoutesRenderData(routes, routeManifest);
 
   const generator = new Generator({
     rootDir,
@@ -50,9 +48,9 @@ async function createService({ rootDir, command, commandArgs, getBuiltInPlugins 
   addWatchEvent([
     path.join(rootDir, 'src'),
     () => {
-      const { routeManifest, routes } = getRouteData(rootDir);
-      const componentsImportStr = generateComponentsImportStr(routeManifest);
-      const routesStr = generateRoutesStr(routes);
+      // will update routes object in context
+      let { routes, routeManifest } = getRoutesData(rootDir);
+      const { componentsImportStr, routesStr } = generateRoutesRenderData(routes, routeManifest);
       generator.renderFile(
         path.join(templatePath, 'routes.ts.ejs'),
         path.join(rootDir, tmpDirName, 'routes.ts'),
@@ -108,45 +106,5 @@ async function createService({ rootDir, command, commandArgs, getBuiltInPlugins 
   };
 }
 
-function getRouteData(rootDir: string) {
-  const routeManifest = generateRouteManifest(rootDir);
-  const routes = formatNestedRouteManifest(routeManifest);
-  return { routeManifest, routes };
-}
-
-
-function generateComponentsImportStr(routeManifest: RouteManifest) {
-  return Object.keys(routeManifest)
-    .reduce((prev: string, id: string) => {
-      let { file, componentName } = routeManifest[id];
-      const fileExtname = path.extname(file);
-      file = file.replace(new RegExp(`${fileExtname}$`), '');
-      return `${prev}const ${componentName} = React.lazy(() => import(/* webpackChunkName: "${componentName}" */ '@/${file}'))\n`;
-  }, '');
-}
-
-function generateRoutesStr(nestRouteManifest: NestedRouteManifest[]) {
-  const str = generateNestRoutesStr(nestRouteManifest);
-  return `[${str}]`;
-}
-
-function generateNestRoutesStr(nestRouteManifest: NestedRouteManifest[]) {
-  return nestRouteManifest.reduce((prev, route) => {
-    const { children, path, index, componentName } = route;
-    let str = `{
-      path: '${path || ''}',
-      component: ${componentName},
-      componentName: '${componentName}',
-      index: ${index},
-      exact: true,
-    `;
-    if (children) {
-      str += `children: [${generateNestRoutesStr(children)}],`;
-    }
-    str += '},';
-    prev += str;
-    return prev;
-  }, '');
-}
 
 export default createService;
---
+++ packages/ice/src/routes.ts
@@ -0,0 +1,50 @@
+import * as path from 'path';
+import type { NestedRouteManifest, RouteManifest } from '@ice/route-manifest';
+import { formatNestedRouteManifest, generateRouteManifest } from '@ice/route-manifest';
+
+export function getRoutesData(rootDir: string) {
+  const routeManifest = generateRouteManifest(rootDir);
+  const routes = formatNestedRouteManifest(routeManifest);
+  return { routeManifest, routes };
+}
+
+export function generateRoutesRenderData(routes: NestedRouteManifest[], routeManifest: RouteManifest) {
+  const componentsImportStr = generateComponentsImportStr(routeManifest);
+  const routesStr = generateRoutesStr(routes);
+
+  return { componentsImportStr, routesStr };
+}
+
+function generateComponentsImportStr(routeManifest: RouteManifest) {
+  return Object.keys(routeManifest)
+    .reduce((prev: string, id: string) => {
+      let { file, componentName } = routeManifest[id];
+      const fileExtname = path.extname(file);
+      file = file.replace(new RegExp(`${fileExtname}$`), '');
+      return `${prev}const ${componentName} = React.lazy(() => import(/* webpackChunkName: "${componentName}" */ '@/${file}'))\n`;
+  }, '');
+}
+
+function generateRoutesStr(nestRouteManifest: NestedRouteManifest[]) {
+  const str = generateNestRoutesStr(nestRouteManifest);
+  return `[${str}]`;
+}
+
+function generateNestRoutesStr(nestRouteManifest: NestedRouteManifest[]) {
+  return nestRouteManifest.reduce((prev, route) => {
+    const { children, path, index, componentName } = route;
+    let str = `{
+      path: '${path || ''}',
+      component: ${componentName},
+      componentName: '${componentName}',
+      index: ${index},
+      exact: true,
+    `;
+    if (children) {
+      str += `children: [${generateNestRoutesStr(children)}],`;
+    }
+    str += '},';
+    prev += str;
+    return prev;
+  }, '');
+}
