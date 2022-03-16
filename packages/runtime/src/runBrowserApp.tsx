a8b63e671b27d7b45fd55dd4a865941b97be8cbd
luhc228 <luhengchang228@gmail.com>
2022-03-16 14:30:23 +0800

    feat: merge render

--- packages/runtime/src/runBrowserApp.tsx
+++ packages/runtime/src/runBrowserApp.tsx
@@ -1,12 +1,59 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HashRouter, BrowserRouter, matchRoutes } from 'react-router-dom';
-import type Runtime from './runtime.js';
+import Runtime from './runtime.js';
 import App from './App.js';
 import AppRoutes from './AppRoutes.js';
-import type { AppRouterProps } from './types';
+import type { AppRouterProps, AppContext, InitialContext, AppConfig } from './types';
 
-export default async function render(runtime: Runtime) {
+export default async function runApp(config: AppConfig, runtimeModules, routes) {
+  const appConfig: AppConfig = {
+    ...config,
+    app: {
+      rootId: 'root',
+      strict: true,
+      ...(config?.app || {}),
+    },
+    router: {
+      type: 'browser',
+      ...(config?.router || {}),
+    },
+  };
+
+  const appContext: AppContext = {
+    routes,
+    appConfig,
+    initialData: null,
+  };
+
+  // ssr enabled and the server has returned data
+  if ((window as any).__ICE_APP_DATA__) {
+    appContext.initialData = (window as any).__ICE_APP_DATA__;
+    // context.pageInitialProps = (window as any).__ICE_PAGE_PROPS__;
+  } else if (appConfig?.app?.getInitialData) {
+    const { href, origin, pathname, search } = window.location;
+    const path = href.replace(origin, '');
+    // const query = queryString.parse(search);
+    const query = {};
+    const ssrError = (window as any).__ICE_SSR_ERROR__;
+    const initialContext: InitialContext = {
+      pathname,
+      path,
+      query,
+      ssrError,
+    };
+    appContext.initialData = await appConfig.app.getInitialData(initialContext);
+  }
+
+  const runtime = new Runtime(appContext);
+  runtimeModules.forEach(m => {
+    runtime.loadModule(m);
+  });
+
+  render(runtime);
+}
+
+async function render(runtime: Runtime) {
   const appContext = runtime.getAppContext();
   const { appConfig, routes } = appContext;
   const { rootId } = appConfig.app;
--- packages/runtime/src/runServerApp.tsx
+++ packages/runtime/src/runServerApp.tsx
@@ -1,6 +1,10 @@
+import * as React from 'react';
+import * as ReactDOMServer from 'react-dom/server.js';
+import { StaticRouter } from 'react-router-dom/server.js';
 import Runtime from './runtime.js';
-import serverRender from './serverRender.js';
-import type { AppContext, AppConfig } from './types';
+import App from './App.js';
+import AppRoutes from './AppRoutes.js';
+import type { AppContext, AppConfig, AppRouterProps } from './types';
 
 export default async function runServerApp(
     requestContext,
@@ -39,5 +43,39 @@ export default async function runServerApp(
     runtime.loadModule(m);
   });
 
-  return serverRender(runtime, requestContext, Document, documentOnly);
+  return render(runtime, requestContext, Document, documentOnly);
+}
+
+async function render(
+  runtime: Runtime,
+  requestContext,
+  Document,
+  documentOnly: boolean,
+) {
+  const documentHtml = ReactDOMServer.renderToString(<Document />);
+
+  if (documentOnly) {
+    return documentHtml;
+  }
+
+  let AppRouter = runtime.getAppRouter();
+  if (!AppRouter) {
+    const { req } = requestContext;
+    AppRouter = (props: AppRouterProps) => (
+      <StaticRouter location={req.url}>
+        <AppRoutes PageWrappers={props.PageWrappers} />
+      </StaticRouter>
+    );
+    runtime.setAppRouter(AppRouter);
+  }
+
+  const pageHtml = ReactDOMServer.renderToString(
+    <App
+      runtime={runtime}
+    />,
+  );
+
+  const html = documentHtml.replace('<!--app-html-->', pageHtml);
+
+  return html;
 }
