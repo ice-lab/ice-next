cdb5e79fdc2b89ef5aca33ecb1d72a7bd2941ccb
ClarkXia <xiawenwu41@gmail.com>
2022-03-31 19:23:33 +0800

    Revert "test: use json file"
    This reverts commit be309d7df5a00fcc14fdc54fe2a63c9bce54221c.

---
+++ examples/basic-project/ice.config.ts
@@ -0,0 +1,13 @@
+import { defineUserConfig } from '@ice/cli';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+export default defineUserConfig({
+  publicPath: '/',
+  webpack: (webpackConfig) => {
+    if (process.env.NODE_ENV !== 'test') {
+      webpackConfig.plugins?.push(new BundleAnalyzerPlugin());
+    }
+    return webpackConfig;
+  },
+  plugins: ['@ice/plugin-auth'],
+});
