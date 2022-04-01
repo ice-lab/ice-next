e39345797abc566b6813c87bc990ad1cc6833ab8
ClarkXia <xiawenwu41@gmail.com>
2022-04-01 14:46:37 +0800

    fix: relative path of runtime

--- packages/ice/src/utils/getRuntimeModules.ts
+++ packages/ice/src/utils/getRuntimeModules.ts
@@ -27,7 +27,7 @@ function getRuntimeModules(plugins: Plugin[]) {
         const pkgInfo = fse.readJSONSync(pkgPath);
         return {
           staticModule: !!pkgInfo?.pluginConfig?.staticModule,
-          path: `${packageDir}/runtime`,
+          path: `${pkgInfo.name}/runtime`,
           name: pkgInfo.name as string,
         };
       } catch (error) {
