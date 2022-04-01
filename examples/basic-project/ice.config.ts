13f5b4936518a7505363f076966f1851b344b316
luhc228 <luhengchang228@gmail.com>
2022-04-01 17:43:47 +0800

    chore: example

--- examples/basic-project/ice.config.ts
+++ examples/basic-project/ice.config.ts
@@ -1,14 +1,14 @@
 import { defineUserConfig } from '@ice/app';
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export default defineUserConfig({
   publicPath: '/',
-  // webpack: (webpackConfig) => {
-  //   if (process.env.NODE_ENV !== 'test') {
-  //     webpackConfig.plugins?.push(new BundleAnalyzerPlugin());
-  //   }
-  //   return webpackConfig;
-  // },
+  webpack: (webpackConfig) => {
+    if (process.env.NODE_ENV !== 'test') {
+      webpackConfig.plugins?.push(new BundleAnalyzerPlugin());
+    }
+    return webpackConfig;
+  },
   routes: {
     ignoreFiles: ['about.tsx'],
     defineRoutes: (route) => {
