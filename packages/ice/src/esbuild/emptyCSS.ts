4cce671bbd2a506fbd782e39f3f0d46c61b51af9
luhc228 <luhengchang228@126.com>
2022-04-29 15:21:46 +0800

    fix: not css module regexp

--- packages/ice/src/esbuild/emptyCSS.ts
+++ packages/ice/src/esbuild/emptyCSS.ts
@@ -10,7 +10,7 @@ const emptyCSSPlugin = (): Plugin => {
     setup: async (build: PluginBuild) => {
       build.onResolve({ filter: /\.(css|sass|scss|less)$/ }, (args) => {
         // golang not support the following regexp, we use javascript to determine again
-        if (/(?<!module)\.(css|sass|scss|less)$/.test(args.path)) {
+        if (/(?<!.module)\.(css|sass|scss|less)$/.test(args.path)) {
           return {
             path: args.path,
             namespace: 'empty-css-content',
