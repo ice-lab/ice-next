2c94c9eaaf59ebe5016db0ee247ed1edbd7b5da5
ClarkXia <xiawenwu41@gmail.com>
2022-05-11 17:44:52 +0800

    feat: analyze route exports

--- packages/ice/src/utils/persistentCache.ts
+++ packages/ice/src/utils/persistentCache.ts
@@ -1,14 +1,14 @@
 import * as path from 'path';
-import { put, get } from '@ice/bundles/compiled/cacache/index.js';
+import cacache from '@ice/bundles/compiled/cacache/index.js';
 
-const CACHE_PATH = 'node_module/.cache/route';
+const CACHE_PATH = 'node_modules/.cache/route';
 
 export function getRouteCache(rootDir: string, routeId: string) {
   const cachePath = path.join(rootDir, CACHE_PATH);
-  return get(cachePath, routeId).then((cache) => JSON.parse(cache.data.toString('utf-8')));
+  return cacache.get(cachePath, routeId).then((cache) => JSON.parse(cache.data.toString('utf-8')));
 }
 
 export function setRouteCache(rootDir: string, routeId: string, data: any) {
   const cachePath = path.join(rootDir, CACHE_PATH);
-  return put(cachePath, routeId, JSON.stringify(data));
+  return cacache.put(cachePath, routeId, JSON.stringify(data));
 }
