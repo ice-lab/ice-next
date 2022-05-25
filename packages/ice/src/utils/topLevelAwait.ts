7f67646416e2b69cea6314279b30bab8e5a57125
ClarkXia <xiawenwu41@gmail.com>
2022-05-25 16:19:37 +0800

    feat: transform toplevelawait

--- packages/ice/src/utils/topLevelAwait.ts
+++ packages/ice/src/utils/topLevelAwait.ts
@@ -4,11 +4,10 @@ import { parse as parseAST } from 'acorn';
 import type { Node } from 'estree';
 import { init, parse } from '@ice/bundles/compiled/es-module-lexer/index.js';
 import type { ImportSpecifier } from '@ice/bundles/compiled/es-module-lexer';
-import { resolveId } from '../service/analyze.js';
-import type { Alias } from '../service/analyze.js';
 
 type ImportName = { importedName: string; localName: string };
-async function topLevelAwait(code: string, alias: Alias) {
+type ValidateId = (id: string) => boolean;
+async function topLevelAwait(code: string, validateId?: ValidateId): Promise<string> {
   await init;
   let imports: readonly ImportSpecifier[] = [];
 
@@ -25,6 +24,7 @@ async function topLevelAwait(code: string, alias: Alias) {
   imports.forEach((importSpecifier) => {
     // check relative import
     let awaitImport = '';
+    if (validateId && !validateId(importSpecifier.n)) return;
     const importStr = code.substring(importSpecifier.ss, importSpecifier.se);
     const node = (parseAST(importStr, {
       ecmaVersion: 'latest',
@@ -32,19 +32,42 @@ async function topLevelAwait(code: string, alias: Alias) {
     }) as any).body[0] as Node;
     if (node.type === 'ImportDeclaration') {
       if (node.specifiers.length === 0) {
-        awaitImport = `import '${node.source.value}';`;
+        awaitImport = `await import('${node.source.value}')`;
       }
 
       const importNames: ImportName[] = [];
       for (const specifier of node.specifiers) {
-        if (specifier.type === 'ImportSpecifier' && specifier.imported.type === 'Identifier') {
+        if (specifier.type === 'ImportNamespaceSpecifier') {
+          // import * as xx from 'xx';
+          awaitImport = `const ${specifier.local.name} = await import('${node.source.value}')`;
+        } else if (specifier.type === 'ImportSpecifier' && specifier.imported.type === 'Identifier') {
           importNames.push({
             importedName: specifier.imported.name,
             localName: specifier.local.name,
           });
-        } else if (specifier.type === 'ImportNamespaceSpecifier') {
+        } else if (specifier.type === 'ImportDefaultSpecifier') {
+          importNames.push({
+            importedName: 'default',
+            localName: specifier.local.name,
+          });
         }
       }
+      const awaitIdentifiers = [];
+      importNames.forEach(({ importedName, localName }) => {
+        if (importedName === localName) {
+          awaitIdentifiers.push(importedName);
+        } else {
+          awaitIdentifiers.push(`${importedName} as ${localName}`);
+        }
+      });
+      if (awaitIdentifiers.length > 0) {
+        awaitImport = `const { ${awaitIdentifiers.join(',')} } = await import('${node.source.value}')`;
+      }
+      str().overwrite(importSpecifier.ss, importSpecifier.se, awaitImport);
     }
   });
+
+  return str().toString();
 }
+
+export default topLevelAwait;
--- packages/ice/tests/topLevelAwait.test.ts
+++ packages/ice/tests/topLevelAwait.test.ts
@@ -1,35 +1,49 @@
 import { expect, it, describe } from 'vitest';
+import topLevelAwait from '../src/utils/topLevelAwait';
 
 describe('top level await transform', () => {
   it('import a from \'a\'', async () => {
-
+    const code = await topLevelAwait('import a from \'a\'');
+    expect(code).toBe(`const { default as a } = await import('a')`);
   });
 
   it('import \'a\'', async () => {
-
+    const code = await topLevelAwait('import \'a\'');
+    expect(code).toBe(`await import('a')`);
   });
 
   it('import { a } from \'a\'', async () => {
-
+    const code = await topLevelAwait('import { a } from \'a\'');
+    expect(code).toBe(`const { a } = await import('a')`);
   });
 
   it('import { a as b } from \'a\'', async () => {
-
+    const code = await topLevelAwait('import { a as b } from \'a\'');
+    expect(code).toBe(`const { a as b } = await import('a')`);
   });
 
   it('import { a, b } from \'a\'', async () => {
-
+    const code = await topLevelAwait('import { a, b } from \'a\'');
+    expect(code).toBe(`const { a,b } = await import('a')`);
   });
 
   it('import { a as c, b } from \'a\'', async () => {
-
+    const code = await topLevelAwait('import { a as c, b } from \'a\'');
+    expect(code).toBe(`const { a as c,b } = await import('a')`);
   });
 
   it('import * as a from \'a\'', async () => {
-
+    const code = await topLevelAwait('import * as a from \'a\'');
+    expect(code).toBe(`const a = await import('a')`);
   });
 
   it('import a, { b } from \'a\'', async () => {
+    const code = await topLevelAwait('import a, { b } from \'a\'');
+    expect(code).toBe(`const { default as a,b } = await import('a')`);
+  });
 
+  it('validate id', async () => {
+    const code = await topLevelAwait('import a, { b } from \'a\'', (id) => false);
+    expect(code).toBe(`import a, { b } from \'a\'`);
   });
 });
