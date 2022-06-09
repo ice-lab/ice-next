4710b173be949a2cebb22a1623a8420a62dbc5e6
luhc228 <luhengchang228@126.com>
2022-06-09 18:02:51 +0800

    chore: remove Dashboard page

--- examples/routes-generate/src/pages/Dashboard/a.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <h3>A page</h3>
-  );
-};
--- examples/routes-generate/src/pages/Dashboard/b.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <h3>B page</h3>
-  );
-};
