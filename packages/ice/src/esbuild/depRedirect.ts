54279ea18a74fcc0aefeb3da79695dd2158c565d
luhc228 <luhengchang228@126.com>
2022-06-09 15:08:19 +0800

    chore: remove unless set.add

--- packages/ice/src/esbuild/depRedirect.ts
+++ packages/ice/src/esbuild/depRedirect.ts
@@ -194,7 +194,6 @@ function transformCjsImport(
 const reservedWords = 'break case class catch const continue debugger default delete do else export extends finally for function if import in instanceof let new return super switch this throw try typeof var void while with yield enum await implements package protected static interface private public';
 const builtins = 'arguments Infinity NaN undefined null true false eval uneval isFinite isNaN parseFloat parseInt decodeURI decodeURIComponent encodeURI encodeURIComponent escape unescape Object Function Boolean Symbol Error EvalError InternalError RangeError ReferenceError SyntaxError TypeError URIError Number Math Date String RegExp Array Int8Array Uint8Array Uint8ClampedArray Int16Array Uint16Array Int32Array Uint32Array Float32Array Float64Array Map Set WeakMap WeakSet SIMD ArrayBuffer DataView JSON Promise Generator GeneratorFunction Reflect Proxy Intl';
 const forbiddenIdentifiers = new Set(`${reservedWords} ${builtins}`.split(' '));
-forbiddenIdentifiers.add('');
 function makeLegalIdentifier(str: string) {
   let identifier = str
     .replace(/-(\w)/g, (_, letter) => letter.toUpperCase())
