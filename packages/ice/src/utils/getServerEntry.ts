94bf740dff663b9ee31a2637fbdd8d6e6612214b
luhc228 <luhengchang228@126.com>
2022-07-26 18:55:39 +0800

    chore: comment

--- packages/ice/src/utils/getServerEntry.ts
+++ packages/ice/src/utils/getServerEntry.ts
@@ -3,13 +3,12 @@ import fg from 'fast-glob';
 import { SERVER_ENTRY } from '../constant.js';
 
 export default function getServerEntry(rootDir: string, customServerEntry?: string) {
-  // check entry.server.ts
+  // Check entry.server.ts.
   let entryFile = fg.sync('entry.server.{tsx,ts,jsx.js}', {
     cwd: path.join(rootDir, 'src'),
     absolute: true,
   })[0];
   if (!entryFile) {
-    // use generated file in template directory
     entryFile = customServerEntry || path.join(rootDir, SERVER_ENTRY);
   }
   return entryFile;
