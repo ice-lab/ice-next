6aa76b3199bf7ad67e673aae3d58823615dba9d6
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-08-11 17:15:31 +0800

    chore: add some description

--- examples/with-plugin-request/src/app.tsx
+++ examples/with-plugin-request/src/app.tsx
@@ -2,19 +2,8 @@ import type { Manifest } from '@ice/plugin-pha/esm/types';
 
 export const phaManifest: Manifest = {
   title: 'test',
-  appWorker: {
-    url: 'app-worker.ts',
-  },
   routes: [
-    {
-      pageHeader: {},
-      frames: [
-        'blog',
-        'home',
-      ],
-    },
     'home',
-    'about',
   ],
 };
 
