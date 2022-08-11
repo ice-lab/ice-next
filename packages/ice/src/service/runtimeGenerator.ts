b3c685bcbbd0381e4f3729e6b7e258536f13251b
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-08-11 18:46:13 +0800

    fix: export statment for generator

--- packages/ice/src/service/runtimeGenerator.ts
+++ packages/ice/src/service/runtimeGenerator.ts
@@ -44,13 +44,13 @@ export function generateExports(exportList: ExportData[]) {
     const specifiers = isDefaultImport ? [specifier] : specifier;
     const symbol = type ? ';' : ',';
     importStatements.push(`import ${type ? 'type ' : ''}${isDefaultImport ? specifier : `{ ${specifier.join(', ')} }`} from '${source}';`);
-    exportStatements.push(specifiers.map((specifierStr) => {
+    specifiers.forEach((specifierStr) => {
       if (exportAlias && exportAlias[specifierStr]) {
-        return `${exportAlias[specifierStr]}: ${specifierStr}${symbol}`;
+        exportStatements.push(`${exportAlias[specifierStr]}: ${specifierStr}${symbol}`);
       } else {
-        return `${specifierStr}${symbol}`;
+        exportStatements.push(`${specifierStr}${symbol}`);
       }
-    }));
+    });
   });
   return {
     importStr: importStatements.join('\n'),
