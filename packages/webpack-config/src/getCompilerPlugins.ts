719dcca82b9e9f419de0ddcf109663e56e755aa1
luhc228 <luhengchang228@126.com>
2022-08-17 16:49:45 +0800

    fix: incorrect sourcemap

--- packages/webpack-config/src/getCompilerPlugins.ts
+++ packages/webpack-config/src/getCompilerPlugins.ts
@@ -3,18 +3,10 @@ import type { BuildOptions } from 'esbuild';
 import { createUnplugin } from 'unplugin';
 import type { UnpluginOptions } from 'unplugin';
 import compilationPlugin from './unPlugins/compilation.js';
+import compileExcludes from './compileExcludes.js';
 import type { WebpackConfig } from './index.js';
 
 type Compiler = 'webpack' | 'esbuild';
-
-const SKIP_COMPILE = [
-  // polyfill and helpers
-  'core-js', 'core-js-pure', '@swc/helpers', '@babel/runtime',
-  // built-in runtime
-  'react', 'react-dom',
-  // dev dependencies
-  '@pmmmwh/react-refresh-webpack-plugin', 'webpack', 'webpack-dev-server', 'react-refresh',
-];
 
 function getPluginTransform(plugin: UnpluginOptions, type: 'esbuild' | 'webpack') {
   const { transform } = plugin;
@@ -46,10 +38,7 @@ function getCompilerPlugins(config: Config, compiler: Compiler) {
     cacheDir,
   } = config;
   const compilerPlugins = [];
-  const compileExcludes = [
-    new RegExp(SKIP_COMPILE.map((dep) => `node_modules/?.+${dep}/`).join('|')),
-    /bundles\/compiled/,
-  ];
+
   // Add custom transform before swc compilation so the source code can be got before transformed.
   compilerPlugins.push(
     ...(transformPlugins.filter(({ enforce }) => !enforce || enforce === 'pre') || []),
