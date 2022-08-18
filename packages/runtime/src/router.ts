88586f6cad5f27ff82fefc37165df260d73a8ddd
ClarkXia <xiawenwu41@gmail.com>
2022-08-18 14:28:21 +0800

    feat: export useNavigate

--- packages/runtime/src/router.ts
+++ packages/runtime/src/router.ts
@@ -4,4 +4,5 @@ export {
   useParams,
   useSearchParams,
   useLocation,
+  useNavigate,
 } from 'react-router-dom';
--- packages/runtime/src/single-router.tsx
+++ packages/runtime/src/single-router.tsx
@@ -1,49 +1,53 @@
 /**
- * 对 react-router-dom 进行二次封装，保证只有一个路由时能够通过 tree-shaking 将 react-router 相关代码全量移除
+ * Fake API of react-router-dom, react-router-dom will be remove
+ * if user config `optimize.router` false
  */
- import * as React from 'react';
- import type { History } from 'history';
+import * as React from 'react';
+import type { History } from 'history';
 
- export const useRoutes = (routes) => {
-   return <>{routes[0].element}</>;
- };
+export const useRoutes = (routes) => {
+  return <>{routes[0].element}</>;
+};
 
- export const Router = (props) => {
-   return <>{props.children}</>;
- };
+export const Router = (props) => {
+  return <>{props.children}</>;
+};
 
- export const createHistory = (): History => {
-   return {
-     // @ts-expect-error
-     listen: () => {},
-     // @ts-expect-error
-     action: 'POP',
-     // @ts-expect-error
-     location: '',
-   };
- };
+export const createHistory = (): History => {
+  return {
+    // @ts-expect-error
+    listen: () => {},
+    // @ts-expect-error
+    action: 'POP',
+    // @ts-expect-error
+    location: '',
+  };
+};
 
- export const matchRoutes = (routes: any[]) => {
-   return routes.map(item => {
-     return {
-       params: {},
-       pathname: '',
-       pathnameBase: '',
-       route: item,
-     };
-   });
- };
+export const matchRoutes = (routes: any[]) => {
+  return routes.map(item => {
+    return {
+      params: {},
+      pathname: '',
+      pathnameBase: '',
+      route: item,
+    };
+  });
+};
 
- export const Link = () => null;
- export const Outlet = () => {
-   return <></>;
- };
- export const useParams = () => {
-   return {};
- };
- export const useSearchParams = () => {
-   return [{}, () => {}];
- };
- export const useLocation = () => {
-   return {};
- };
+export const Link = () => null;
+export const Outlet = () => {
+  return <></>;
+};
+export const useParams = () => {
+  return {};
+};
+export const useSearchParams = () => {
+  return [{}, () => {}];
+};
+export const useLocation = () => {
+  return {};
+};
+export const useNavigate = () => {
+  return {};
+};
