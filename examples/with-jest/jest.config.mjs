96dd31a63781c30e9a49a210bd23dc75b7fb5bac
luhc228 <luhengchang228@126.com>
2022-09-07 11:20:46 +0800

    feat: jest component test

--- examples/with-jest/jest.config.mjs
+++ examples/with-jest/jest.config.mjs
@@ -2,4 +2,6 @@ import { defineJestConfig } from '@ice/app';
 
 export default defineJestConfig({
   preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
 });
--- examples/with-jest/src/pages/index.tsx
+++ examples/with-jest/src/pages/index.tsx
@@ -1,5 +1,7 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <div>Jest Test</div>
+    <Header />
   );
 }
