cff55f0ebc6e4754dc4bf9f206073b7a6f1a71c1
luhc228 <luhengchang228@126.com>
2022-09-14 14:58:28 +0800

    fix: not depend on the vitest as dependencies

--- packages/ice/src/test/defineVitestConfig.ts
+++ packages/ice/src/test/defineVitestConfig.ts
@@ -1,4 +1,3 @@
-import { defineConfig } from 'vitest/config';
 import type { UserConfigExport, ConfigEnv, UserConfig } from 'vitest/config';
 import lodash from '@ice/bundles/compiled/lodash/index.js';
 import getTaskConfig from './getTaskConfig.js';
@@ -6,7 +5,7 @@ import getTaskConfig from './getTaskConfig.js';
 const { merge } = lodash;
 
 export default function defineVitestConfig(userConfig: UserConfigExport): UserConfigExport {
-  return defineConfig(async (env: ConfigEnv) => {
+  return async (env: ConfigEnv) => {
     // Support vitest configuration (object or function) Ref: https://github.com/vitest-dev/vitest/blob/e5c40cff0925c3c12d8cdfa59f5649d3562668ce/packages/vitest/src/config.ts#L3
     let customConfig: UserConfig;
     if (typeof userConfig === 'function') {
@@ -18,7 +17,7 @@ export default function defineVitestConfig(userConfig: UserConfigExport): UserCo
     const defaultConfig = await getDefaultConfig();
 
     return merge(defaultConfig, customConfig);
-  });
+  };
 }
 
 async function getDefaultConfig() {
