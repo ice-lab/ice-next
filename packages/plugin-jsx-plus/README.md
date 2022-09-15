42bfbc3444c9dcc55e26e5ee32ffda6d7d072b11
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-09-15 18:57:41 +0800

    feat: create plugin jsx plus

---
+++ packages/plugin-jsx-plus/README.md
@@ -0,0 +1,19 @@
+# `plugin-jsx-plus`
+
+This plugin adds support for JSX+ syntax to the icejs 3.
+
+# Definition of JSX Plus
+https://github.com/jsx-plus/jsx-plus
+
+# Usage
+
+```js
+import { defineConfig } from '@ice/app';
+import jsxplus from '@ice/plugin-jsx-plus';
+
+export default defineConfig({
+  plugins: [
+    jsxplus(),
+  ],
+});
+```
---
+++ packages/plugin-jsx-plus/src/index.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import type { Plugin } from '@ice/types';
+
+const plugin: Plugin = () => ({
+  name: '@ice/plugin-jsx-plus',
+  setup: ({ generator }) => {
+  // Register API: `import { useAuth, withAuth } from 'ice';`
+    generator.addExport({
+      specifier: ['withAuth', 'useAuth'],
+      source: '@ice/plugin-auth/runtime/Auth',
+    });
+  },
+  runtime: path.join(path.dirname(fileURLToPath(import.meta.url)), 'runtime', 'index.js'),
+});
+
+export default plugin;
