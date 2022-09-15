2cd8da88e3385dced105436004e3dfef130e8580
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-09-15 19:50:55 +0800

    feat: add jsx plus plugin

---
+++ examples/rax-project/src/pages/jsxplus.jsx
@@ -0,0 +1,24 @@
+import { createElement } from 'rax';
+
+export default function JSXPlusDemo() {
+  const list = [0, 1, 2, 3];
+  const val = 'foo';
+  return (
+    // x-class
+    <div x-class={{ item: true, active: val }}>
+      {/* x-if */}
+      <div x-if>Should Show</div>
+      <div x-if={false}>Should Hide</div>
+
+      {/* x-for */}
+      <span x-for={item in list} key={item}> {item} </span>
+
+      {/* Fragment */}
+      <>
+        <div>Fragment 1</div>
+        <div>Fragment 2</div>
+        <div>Fragment 3</div>
+      </>
+    </div>
+  );
+}
--- packages/plugin-jsx-plus/src/index.ts
+++ packages/plugin-jsx-plus/src/index.ts
@@ -1,17 +1,92 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
 import type { Plugin } from '@ice/types';
+import { transformSync } from '@babel/core';
+
+const require = createRequire(import.meta.url);
+const runtimePackage = 'babel-runtime-jsx-plus';
+const runtimePackagePath = require.resolve(runtimePackage);
+
+const babelPlugins = [
+  'babel-plugin-transform-jsx-list',
+  'babel-plugin-transform-jsx-condition',
+  'babel-plugin-transform-jsx-memo',
+  'babel-plugin-transform-jsx-slot',
+  ['babel-plugin-transform-jsx-fragment', { moduleName: 'react' }],
+  'babel-plugin-transform-jsx-class',
+];
+
+const babelTransformOptions = {
+  babelrc: false,
+  configFile: false,
+  parserOpts: {
+    sourceType: 'module',
+    allowAwaitOutsideFunction: true,
+    // ts syntax had already been transformed by swc plugin.
+    plugins: [
+      'jsx',
+      'importMeta',
+      'topLevelAwait',
+      'classProperties',
+      'classPrivateMethods',
+    ],
+    generatorOpts: {
+      decoratorsBeforeExport: true,
+    },
+  },
+  plugins: [],
+};
+
+babelPlugins.forEach((plugin) => {
+  if (typeof plugin === 'string') {
+    babelTransformOptions.plugins.push(require.resolve(plugin));
+  } else if (Array.isArray(plugin)) {
+    const pluginName = plugin[0] as string;
+    const pluginOption = plugin[1];
+    babelTransformOptions.plugins.push([require.resolve(pluginName), pluginOption]);
+  }
+});
 
 const plugin: Plugin = () => ({
   name: '@ice/plugin-jsx-plus',
-  setup: ({ generator }) => {
-  // Register API: `import { useAuth, withAuth } from 'ice';`
-    generator.addExport({
-      specifier: ['withAuth', 'useAuth'],
-      source: '@ice/plugin-auth/runtime/Auth',
+  setup: ({ onGetConfig, context }) => {
+    const sourceDir = path.join(context.rootDir, 'src');
+
+    function jsxPlusTransformer(source, id) {
+      // Filter that 'src' files that to be executed.
+      if (id.startsWith(sourceDir) && /\.[tj]sx$/.test(id)) {
+        try {
+          const options = Object.assign({
+            filename: id,
+            sourceFileName: id,
+          }, babelTransformOptions);
+          if (/\.tsx?$/.test(id)) {
+            // when routes file is a typescript file,
+            // add ts parser plugins
+            options.parserOpts.plugins.push('typescript');
+            options.parserOpts.plugins.push('decorators-legacy'); // allowing decorators by default
+          }
+
+          const { code, map } = transformSync(source, options);
+          return { code, map };
+        } catch (compileError) {
+          console.error(compileError);
+          return { code: source, map: null };
+        }
+      }
+      return { code: source, map: null };
+    }
+
+    onGetConfig((config) => {
+      // Add runtime alias.
+      config.alias ??= {};
+      config.alias[runtimePackage] = runtimePackagePath;
+
+      // Apply babel jsx plus transformer.
+      config.transforms ??= [];
+      config.transforms.push(jsxPlusTransformer);
     });
   },
-  runtime: path.join(path.dirname(fileURLToPath(import.meta.url)), 'runtime', 'index.js'),
 });
 
 export default plugin;
