0a457e061219e07e80e2b62f440455a2daa29f07
luhc228 <luhengchang228@126.com>
2022-09-26 11:34:03 +0800

    fix: not compatible with relative path

--- packages/ice/src/utils/getRuntimeModules.ts
+++ packages/ice/src/utils/getRuntimeModules.ts
@@ -20,35 +20,56 @@ function getRuntimeModules(plugins: Array<PluginInfo<any, ExtendsPluginAPI>>, ro
     .filter(({ runtime }) => !!runtime)
     .map(({ name, runtime }) => ({ name, runtime }));
   return runtimes.map(({ runtime, name }) => {
-    // for example: xx/build-plugin-app/package.json
-    const pkgPath = findUp.sync('package.json', { cwd: path.join(rootDir, 'node_modules', name) });
-    // for example: xx/build-plugin-app/
-    const packageDir = path.dirname(pkgPath);
+    let runtimeExists = false;
     let pkgInfo: Record<string, any>;
-    try {
-      pkgInfo = fse.readJSONSync(pkgPath);
-    } catch (error) {
-      consola.error(`Failed to load package.json of plugin ${path.basename(packageDir)}`);
-      return false;
+
+    if (runtime.startsWith('.')) {
+      // The runtime path is relative and it exists in the local project directory not in the node_modules.
+      // for example: ./runtime
+      runtimeExists = fse.pathExistsSync(path.join(rootDir, runtime));
+      pkgInfo = getPkgInfo(path.join(rootDir, 'package.json'));
+    } else {
+      // The runtime is in the node_modules.
+      let pluginName = '';
+      if (runtime.startsWith('@')) {
+        // @ice/plugin-auth
+        pluginName = runtime.split('/').slice(0, 2).join('/');
+      } else {
+        // plugin-auth
+        pluginName = runtime.split('/')[0];
+      }
+      // for example: xx/@ice/plugin-auth/package.json
+      const pkgPath = findUp.sync('package.json', { cwd: path.join(rootDir, 'node_modules', pluginName) });
+      pkgInfo = getPkgInfo(pkgPath);
+
+      try {
+        runtimeExists = !!(resolveExports(pkgInfo || {}, runtime) || require.resolve(runtime, { paths: [rootDir] }));
+      } catch (error) {
+        // ignore error
+      }
     }
 
-    let runtimeExists = false;
-    try {
-      runtimeExists = !!(resolveExports(pkgInfo, runtime) || require.resolve(runtime, { paths: [rootDir] }));
-    } catch (error) {
-      // ignore error
-    }
     if (runtimeExists) {
       return {
         staticModule: !!pkgInfo?.pluginConfig?.staticModule,
         path: runtime,
-        name: pkgInfo.name as string,
+        name: pkgInfo?.name as string,
       };
     } else {
       consola.warn(`runtime is not exist in ${name}`);
     }
     return false;
   }).filter(Boolean) as RuntimeModule[];
+}
+
+function getPkgInfo(pkgPath: string) {
+  try {
+    return fse.readJSONSync(pkgPath);
+  } catch (error) {
+    // for example: xx/build-plugin-app/
+    const packageDir = path.dirname(pkgPath);
+    consola.error(`Failed to load package.json of plugin ${path.basename(packageDir)}`);
+  }
 }
 
 export default getRuntimeModules;
