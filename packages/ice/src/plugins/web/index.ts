47055e030918a04a0def54475073ace558115b1a
ClarkXia <xiawenwu41@gmail.com>
2022-09-27 10:17:24 +0800

    chore: remove dead code

--- packages/ice/src/plugins/web/index.ts
+++ packages/ice/src/plugins/web/index.ts
@@ -1,59 +1,14 @@
 import * as path from 'path';
 import consola from 'consola';
 import chalk from 'chalk';
-import type { Config } from '@ice/types';
-import type { Context, TaskConfig } from 'build-scripts';
-import type { ServerCompiler, ExtendsPluginAPI } from '@ice/types/esm/plugin.js';
-import ServerCompilerPlugin from '../../webpack/ServerCompilerPlugin.js';
 import ReCompilePlugin from '../../webpack/ReCompilePlugin.js';
 import DataLoaderPlugin from '../../webpack/DataLoaderPlugin.js';
 import { getRouteExportConfig } from '../../service/config.js';
 import { WEB, SERVER_OUTPUT_DIR } from '../../constant.js';
 import getWebTask from '../../tasks/web/index.js';
-import getServerEntry from '../../utils/getServerEntry.js';
-import { getRoutePathsFromCache } from '../../utils/getRoutePaths.js';
 import generateHTML from '../../utils/generateHTML.js';
 import openBrowser from '../../utils/openBrowser.js';
 import getServerCompilerPlugin from '../../utils/getServerCompilerPlugin.js';
-
-const getServerCompilerOptions = ({
-  rootDir,
-  userConfig,
-  taskConfigs,
-  dataCache,
-  serverOutfile,
-}: {
-  rootDir: string;
-  serverOutfile: string;
-  userConfig: Context<Config, ExtendsPluginAPI>['userConfig'];
-  taskConfigs: TaskConfig<Config>[];
-  dataCache: Map<string, string>;
-}): Parameters<ServerCompiler> => {
-  const { ssg, ssr, server: { format } } = userConfig;
-  const entryPoint = getServerEntry(rootDir, taskConfigs[0].config?.server?.entry);
-  const esm = format === 'esm';
-
-  return [
-    {
-      entryPoints: { index: entryPoint },
-      outdir: path.dirname(serverOutfile),
-      splitting: esm,
-      format,
-      platform: esm ? 'browser' : 'node',
-      outExtension: { '.js': format === 'esm' ? '.mjs' : '.cjs' },
-    },
-    {
-      preBundle: format === 'esm' && (ssr || ssg),
-      swc: {
-        keepExports: (!ssg && !ssr) ? ['getConfig'] : null,
-        keepPlatform: 'node',
-        getRoutePaths: () => {
-          return getRoutePathsFromCache(dataCache);
-        },
-      },
-    },
-  ];
-};
 
 const plugin = ({ registerTask, onHook, context }) => {
   const { rootDir, commandArgs, command, userConfig, extendsPluginAPI: { serverCompileTask, dataCache } } = context;
@@ -67,17 +22,6 @@ const plugin = ({ registerTask, onHook, context }) => {
     const outputDir = webpackConfigs[0].output.path;
     serverOutfile = path.join(outputDir, SERVER_OUTPUT_DIR, `index${format === 'esm' ? '.mjs' : '.cjs'}`);
     webpackConfigs[0].plugins.push(
-      new ServerCompilerPlugin(
-        serverCompiler,
-        getServerCompilerOptions({
-          rootDir,
-          serverOutfile,
-          userConfig,
-          taskConfigs,
-          dataCache,
-        }),
-        serverCompileTask,
-      ),
       // Add webpack plugin of data-loader in web task
       new DataLoaderPlugin({ serverCompiler, rootDir, dataCache }),
       // Add ServerCompilerPlugin
