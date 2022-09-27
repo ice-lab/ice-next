43f38bdda8d21101893f7499cd3e68ece0c55b8e
luhc228 <luhengchang228@126.com>
2022-09-27 13:28:48 +0800

    test: add defineVitestConfig test case

--- packages/ice/tests/defineJestConfig.test.ts
+++ packages/ice/tests/defineJestConfig.test.ts
@@ -11,27 +11,27 @@ describe('defineJestConfig', () => {
     spy.mockReturnValue(path.join(__dirname, '../../../examples/with-jest'));
   });
 
-  it('get default config with a param of an empty object', async () => {
+  it('get default config with object', async () => {
     const jestConfigFn = defineJestConfig({});
     const jestConfig = await jestConfigFn();
     expect(Object.keys(jestConfig)).toStrictEqual(['moduleNameMapper']);
     expect(Object.keys(jestConfig.moduleNameMapper as Record<string, string>)).toStrictEqual(['^ice', '^@/(.*)', '^webpack/hot', '^regenerator-runtime']);
   });
 
-  it('get default config with a param of a function with empty object', async () => {
+  it('get default config with function', async () => {
     const jestConfigFn = defineJestConfig(async () => { return {}; });
     const jestConfig = await jestConfigFn();
     expect(Object.keys(jestConfig)).toStrictEqual(['moduleNameMapper']);
     expect(Object.keys(jestConfig.moduleNameMapper as Record<string, string>)).toStrictEqual(['^ice', '^@/(.*)', '^webpack/hot', '^regenerator-runtime']);
   });
 
-  it('get default config with custom config object', async () => {
+  it('get config with custom object config', async () => {
     const jestConfigFn = defineJestConfig({ testTimeout: 12000 });
     const jestConfig = await jestConfigFn();
     expect(Object.keys(jestConfig)).toStrictEqual(['moduleNameMapper', 'testTimeout']);
   });
 
-  it('get default config with custom config function', async () => {
+  it('get config with custom function config', async () => {
     const jestConfigFn = defineJestConfig(async () => { return { testTimeout: 12000 }; });
     const jestConfig = await jestConfigFn();
     expect(Object.keys(jestConfig)).toStrictEqual(['moduleNameMapper', 'testTimeout']);
