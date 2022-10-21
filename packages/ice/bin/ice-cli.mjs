6065e616f5b102376abca032665a82cc18062911
luhc228 <luhengchang228@126.com>
2022-10-21 12:15:25 +0800

    chore: remove platform options

--- packages/ice/bin/ice-cli.mjs
+++ packages/ice/bin/ice-cli.mjs
@@ -2,13 +2,12 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fse from 'fs-extra';
-import { program, Option } from 'commander';
+import { program } from 'commander';
 import detectPort from 'detect-port';
 // hijack webpack before import other modules
 import '../esm/requireHook.js';
 import createService from '../esm/createService.js';
 import checkNodeVersion from './checkNodeVersion.mjs';
-import { ALL_PLATFORMS } from '../esm/constant.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -18,15 +17,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
   process.env.__ICE_VERSION__ = icePackageInfo.version;
   const cwd = process.cwd();
 
-  program
-    .version(icePackageInfo.version)
-    .usage('<command> [options]');
+  program.version(icePackageInfo.version).usage('<command> [options]');
 
   program
     .command('build')
     .description('build project')
     .allowUnknownOption()
-    .addOption(new Option('--platform <platform>', 'set platform').default('web').choices(ALL_PLATFORMS))
+    .option('--platform <platform>', 'set platform', 'web')
     .option('--mode <mode>', 'set mode', 'production')
     .option('--analyzer', 'visualize size of output files', false)
     .option('--config <config>', 'use custom config')
@@ -41,7 +38,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
     .command('start')
     .description('start server')
     .allowUnknownOption()
-    .addOption(new Option('--platform <platform>', 'set platform').default('web').choices(ALL_PLATFORMS))
+    .option('--platform <platform>', 'set platform', 'web')
     .option('--mode <mode>', 'set mode', 'development')
     .option('--config <config>', 'custom config path')
     .option('-h, --host <host>', 'dev server host', '0.0.0.0')
--- packages/ice/src/constant.ts
+++ packages/ice/src/constant.ts
@@ -17,17 +17,3 @@ export const BUILDIN_CJS_DEPS = [
 ];
 
 export const WEB = 'web';
-export const ALI_MINIAPP = 'ali-miniapp';
-export const WECHAT_MINIPROGRAM = 'wechat-miniprogram';
-export const BYTEDANCE_MICROAPP = 'bytedance-microapp';
-export const BAIDU_SMARTPROGRAM = 'baidu-smartprogram';
-export const KUAISHOU_MINIPROGRAM = 'kuaishou-miniprogram';
-
-export const MINIAPP_PLATFORMS = [
-  ALI_MINIAPP, WECHAT_MINIPROGRAM, BYTEDANCE_MICROAPP,
-  BAIDU_SMARTPROGRAM, KUAISHOU_MINIPROGRAM,
-];
-export const ALL_PLATFORMS = [
-  WEB,
-  ...MINIAPP_PLATFORMS,
-];
