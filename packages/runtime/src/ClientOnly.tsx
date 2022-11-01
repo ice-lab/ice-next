7647901e5cc601d7113f0ea9d7b24ac846b08355
ClarkXia <xiawenwu41@gmail.com>
2022-11-01 15:57:25 +0800

    chore: optimize code

--- packages/runtime/src/ClientOnly.tsx
+++ packages/runtime/src/ClientOnly.tsx
@@ -1,9 +1,11 @@
 import React, { isValidElement } from 'react';
 import useMounted from './useMounted.js';
 
-const ClientOnly: React.FC<{fallback: React.ReactElement; children: () => React.ReactNode }> = ({ children, fallback }) => {
+const ClientOnly: React.FC<{
+  fallback: React.ReactElement;
+  children: () => React.ReactNode;
+}> = ({ children, fallback }) => {
   const mounted = useMounted();
-
   // Ref https://github.com/facebook/docusaurus/blob/v2.1.0/packages/docusaurus/src/client/exports/BrowserOnly.tsx
   if (mounted) {
     if (
