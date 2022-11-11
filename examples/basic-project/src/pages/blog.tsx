f1a4db7f11db5972d37923a4183d7213a8faca42
ClarkXia <xiawenwu41@gmail.com>
2022-11-11 11:57:29 +0800

    chore: optimize code

--- examples/basic-project/src/pages/blog.tsx
+++ examples/basic-project/src/pages/blog.tsx
@@ -1,4 +1,18 @@
-import { definePageConfig } from 'ice';
+import { Link, useData, useConfig, definePageConfig } from 'ice';
+
+export default function Blog() {
+  const data = useData();
+  const config = useConfig();
+
+  console.log('render Blog', 'data', data, 'config', config);
+
+  return (
+    <>
+      <h2>Blog Page</h2>
+      <Link to="/">home</Link>
+    </>
+  );
+}
 
 export const pageConfig = definePageConfig(() => {
   return {
